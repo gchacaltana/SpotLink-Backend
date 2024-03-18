a71336e40934ce2faba001d1ee00dfda5625669a
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-03-18 01:24:52 -0500

    update Controller - add method for validate auth user

--- shortURLServiceAPI/app/Http/Controllers/Controller.php
+++ shortURLServiceAPI/app/Http/Controllers/Controller.php
@@ -5,8 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\Exceptions\AuthException;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function validateAuthUser()
+    {
+        if (is_null(auth()->user())) {
+            throw new AuthException('Unauthorized');
+        }
+    }
 }
