5b641d25152e782730bfe380755bde4ac8b7a195
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2024-03-19 12:34:25 -0500

    update routes web - return json for home api

--- shortURLServiceAPI/routes/web.php
+++ shortURLServiceAPI/routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'api' => [
+            'name' => 'SpotLink API',
+            'version' => '1.0.0',
+            'description' => 'API handles the creation and redirection of short URLs'
+        ]
+    ]);
 });
